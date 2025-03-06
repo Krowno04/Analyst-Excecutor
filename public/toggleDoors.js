298941208a98b86c5d147a5635f346d253a072df
Kro <124914389+Krowno04@users.noreply.github.com>
2025-03-05 19:19:52 -0500

    Door toggles and gold door included

--- public/toggleDoors.js
+++ public/toggleDoors.js
@@ -1,33 +1,38 @@
 socket.on('connect', (userData) => {
+    document.querySelector("#gold_door").setAttribute('visible', true);
+    document.querySelector("#red_doors").setAttribute('visible', true);
+    document.querySelector("#green_doors").setAttribute('visible', true);
+    document.querySelector("#blue_doors").setAttribute('visible', true);
+    console.log("I LIVE")
 })
-AFRAME.registerComponent('toggle1', {
-    init: function () {
-        //listen to event from server
-        socket.on('red', (data) => {
-            if (data == true) {
-                document.querySelector("red_doors").setAttribute('visible', true);
-                document.querySelector("green_doors").setAttribute('visible', false);
-                document.querySelector("blue_doors").setAttribute('visible', false);
-            }
-            console.log('Red Toggled');
-        });
-        //listen to event from server
-        socket.on('green', (data) => {
-            if (data == true) {
-                document.querySelector("red_doors").setAttribute('visible', false);
-                document.querySelector("green_doors").setAttribute('visible', true);
-                document.querySelector("blue_doors").setAttribute('visible', false);
-            }
-            console.log('Green Toggled');
-        });
-        //listen to event from server
-        socket.on('blue', (data) => {
-            if (data == true) {
-                document.querySelector("red_doors").setAttribute('visible', false);
-                document.querySelector("green_doors").setAttribute('visible', false);
-                document.querySelector("blue_doors").setAttribute('visible', true);
-            }
-            console.log('Blue Toggled');
-        });
-    }
-})
+//listen to event from server
+socket.on('red_door', (data) => {
+
+    document.querySelector("#red_doors").setAttribute('visible', false);
+    document.querySelector("#green_doors").setAttribute('visible', true);
+    document.querySelector("#blue_doors").setAttribute('visible', true);
+    console.log('Red Toggled');
+});
+//listen to event from server
+socket.on('green_door', (data) => {
+    document.querySelector("#red_doors").setAttribute('visible', true);
+    document.querySelector("#green_doors").setAttribute('visible', false);
+    document.querySelector("#blue_doors").setAttribute('visible', true);
+    console.log('Green Toggled');
+});
+//listen to event from server
+socket.on('blue_door', (data) => {
+
+    document.querySelector("#red_doors").setAttribute('visible', true);
+    document.querySelector("#green_doors").setAttribute('visible', true);
+    document.querySelector("#blue_doors").setAttribute('visible', false);
+    console.log('Blue Toggled');
+});
+socket.on('gold_door', (data) => {
+
+    document.querySelector("#red_doors").setAttribute('visible', false);
+    document.querySelector("#green_doors").setAttribute('visible', false);
+    document.querySelector("#blue_doors").setAttribute('visible', false);
+    document.querySelector("#gold_door").setAttribute('visible', false);
+    console.log('GOLD Toggled');
+});
